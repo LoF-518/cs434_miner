12c97d30727a246554a559b9f9cb7c75d6d25518
LoF-518 <mukul.potta@iitgn.ac.in>
2024-04-16 22:20:50 +0530

    Ran the code

--- start.sh
+++ start.sh
@@ -4,6 +4,6 @@ echo "Analyzing Drivers for Java..."
 ./main.sh java_mongodb_drivers.csv $1
 ./main.sh java_cass_drivers.csv $1
 
-# echo "Analyzing Drivers for Python..."
+echo "Analyzing Drivers for Python..."
 ./main.sh py_mongodb_drivers.csv $1
 ./main.sh py_cass_drivers.csv $1
